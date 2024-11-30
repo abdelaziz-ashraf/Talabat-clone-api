b854078f874106375dfcba243f4db73242b13815
Abdelaziz Ashraf <abdelazizasf@gmail.com>
2024-11-30 16:25:00 +0200

    orgainze routes

--- routes/api.php
+++ routes/api.php
@@ -23,25 +23,31 @@ Route::prefix('vendor')->group(function () {
         Route::get('/', 'index');
         Route::get('/{vendor}/menu', 'menu');
         Route::get('/{vendor}', 'show');
-        Route::put('/', 'update');
-        Route::delete('/', 'destroy');
     });
 
     Route::get('/{vendor}/categories', [CategoryController::class, 'getVendorCategories']);
-    Route::prefix('categories')->group(function () {
-        Route::get('/{category}', [CategoryController::class, 'show']);
-        Route::post('/', [CategoryController::class, 'store']);
-        Route::middleware(CheckVendorOwnsCategoryMiddleware::class)->group(function () {
-            Route::put('/{category}', [CategoryController::class, 'update']);
-            Route::delete('/{category}', [CategoryController::class, 'destroy']);
-        });
-    });
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
-    Route::prefix('products')->group(function () {
-        Route::post('/', [ProductController::class, 'store']);
-        Route::middleware(CheckVendorOwnsProductMiddleware::class)->group(function () {
-            Route::put('/{product}', [ProductController::class, 'update']);
-            Route::delete('/{product}', [ProductController::class, 'destroy']);
+    Route::middleware('auth:vendor')->group(function () {
+        Route::controller(VendorController::class)->group(function () {
+            Route::put('/', 'update');
+            Route::delete('/', 'destroy');
+        });
+
+        Route::prefix('categories')->group(function () {
+            Route::post('/', [CategoryController::class, 'store']);
+            Route::middleware(CheckVendorOwnsCategoryMiddleware::class)->group(function () {
+                Route::put('/{category}', [CategoryController::class, 'update']);
+                Route::delete('/{category}', [CategoryController::class, 'destroy']);
+            });
+        });
+
+        Route::prefix('products')->group(function () {
+            Route::post('/', [ProductController::class, 'store']);
+            Route::middleware(CheckVendorOwnsProductMiddleware::class)->group(function () {
+                Route::put('/{product}', [ProductController::class, 'update']);
+                Route::delete('/{product}', [ProductController::class, 'destroy']);
+            });
         });
     });
 });
